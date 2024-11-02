8019abda12cc87616cc948067ec7705f8ab18d26
fred <domaine75@gmail.com>
2024-11-03 00:07:08 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppGitInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleAppGitInfo
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine(Git.Commit);
+
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}

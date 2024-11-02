4ccb4a7962b779ccd2a7485396ca12adc18308f5
fred <domaine75@gmail.com>
2024-11-03 00:14:35 +0100

    add .NET 8.0 console app

--- ConsoleAppgitInfoNet8/Program.cs
+++ ConsoleAppgitInfoNet8/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace ConsoleAppGitInfo
+﻿namespace ConsoleAppgitInfoNet8
 {
   internal class Program
   {
     static void Main()
     {
-      //Console.WriteLine(ThisAssembly.Git.Commit);
+      Console.WriteLine(ThisAssembly.Git.Commit);
 
       Console.WriteLine("Press any key to exit:");
       Console.ReadKey();
